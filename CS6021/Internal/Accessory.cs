e95404fb517014661448a8ba99cbd2c0a2ae65d8
Ralf_ <91413753+RalfMende@users.noreply.github.com>
2022-09-10 22:30:20 +0200

    Added License disclaimer in files

--- CS6021/Internal/Accessory.cs
+++ CS6021/Internal/Accessory.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/*
+ * CS6021 - iPad Remote Controller for Märklin Central Station in ControlUnit-Design
+ * 
+ * Copyright (c) 2022 Ralf Mende
+ * 
+ * 
+ * This file is part of CS6021.
+ * 
+ * CS6021 is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the 
+ * Free Software Foundation, either version 3 of the License, or (at your 
+ * option) any later version.
+ * 
+ * CS6021 is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * along with Foobar. If not, see 
+ * <https://www.gnu.org/licenses/>.
+*/
+
+using System;
 
 namespace Internal
 {
